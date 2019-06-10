171c3ee68b120c6ee4e3123b7db3390581a1314b
Ronne <mr.ronne@yandex.ru>
2019-06-10 11:13:23 +0500

    Перенёс обновления с ветки MonoSolution

--- Client/Forms/MainMenuForm.cs
+++ Client/Forms/MainMenuForm.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Drawing;
+using System.Net;
+using System.Net.Sockets;
 using System.Runtime.Remoting.Channels;
 using System.Windows.Forms;
 using GameData.ClientInteraction;
+using GameData.Packets;
 
 namespace Client.Forms
 {
@@ -21,81 +24,76 @@ namespace Client.Forms
 
             var isRed = true;
 
-            /*
-            var playLabel = new Label();
-            playLabel.AutoSize = false;
-            playLabel.Location = new Point(0, 0);
-            playLabel.Size = new Size(ClientSize.Width, 64);
-            playLabel.BackColor = Color.Transparent;
-            playLabel.ForeColor = Color.White;
-            playLabel.Font = new Font(Visual.FontName, 16);
-            playLabel.TextAlign = ContentAlignment.MiddleCenter;
-            playLabel.Text = @"Play";
-            Controls.Add(playLabel);
-            */
-            var engageButton = new Button();
-            var preferredSideLabel = new Label();
-            var choseSideButton = new Button();
+            var engageButton = CreateButton(0, @"Engage");
+            var preferredSideLabel = CreateLabel(engageButton.Bottom, @"Preferred side:");
+            var preferredSideButton = CreateButton(preferredSideLabel.Bottom);
+            var nameLabel = CreateLabel(preferredSideButton.Bottom, @"Name:");
+            var nameText = CreateTextBox(nameLabel.Bottom, @"Player");
+            var serverIPLabel = CreateLabel(nameText.Bottom, @"Server IP:");
+            var serverIPText = CreateTextBox(serverIPLabel.Bottom, @"127.0.0.1");
 
-            engageButton.AutoSize = false;
-            engageButton.Location = new Point(0, ClientSize.Height / 3);
-            engageButton.Size = new Size(ClientSize.Width, 32);
-            engageButton.BackColor = Color.Black;
-            engageButton.ForeColor = Color.White;
-            engageButton.Font = new Font(Visual.FontName, 16);
-            engageButton.TextAlign = ContentAlignment.MiddleCenter;
-            engageButton.Text = @"Engage";
             engageButton.Click += (sender, args) =>
             {
-                var gameForm = new GameForm(
+
+                var gameSession = new GameSession(
                     new ControlSettings(Keys.ControlKey, Keys.ShiftKey, Keys.Right, Keys.Left, Keys.Up, Keys.Down),
                     isRed,
                     11,
+                    10,
                     10);
                 Visible = false;
-                gameForm.ShowDialog(this);
-                preferredSideLabel.Dispose();
-                choseSideButton.Dispose();
-                engageButton.Dispose();
-                Dispose();
-            };
-            Controls.Add(engageButton);
+                gameSession.GameForm.ShowDialog(this);
 
-            preferredSideLabel.AutoSize = false;
-            preferredSideLabel.Location = new Point(0, engageButton.Bottom);
-            preferredSideLabel.Size = new Size(ClientSize.Width, 64);
-            preferredSideLabel.BackColor = Color.Transparent;
-            preferredSideLabel.ForeColor = Color.White;
-            preferredSideLabel.Font = new Font(Visual.FontName, 16);
-            preferredSideLabel.TextAlign = ContentAlignment.MiddleCenter;
-            preferredSideLabel.Text = @"Preferred side";
-            Controls.Add(preferredSideLabel);
+                /*
+                var connectingLabel = CreateLabel(0);
+                var ipIsCorrect = IPAddress.TryParse(serverIPText.Text, out var ip);
+                connectingLabel.Text = ipIsCorrect ? $@"Connecting to {ip}" : @"Wrong IP";
+                Controls.Add(connectingLabel);
 
-            choseSideButton.AutoSize = false;
-            choseSideButton.Location = new Point(0, preferredSideLabel.Bottom);
-            choseSideButton.Size = new Size(ClientSize.Width, 32);
-            choseSideButton.BackColor = isRed ? Color.DarkRed : Color.DarkBlue;
-            choseSideButton.ForeColor = Color.White;
-            choseSideButton.Font = new Font(Visual.FontName, 16);
-            choseSideButton.TextAlign = ContentAlignment.MiddleCenter;
-            choseSideButton.Text = @"Red";
-            
-            choseSideButton.Click += (sender, args) =>
-            {
-                if (isRed)
+                if (ipIsCorrect)
                 {
-                    choseSideButton.BackColor = Color.DarkBlue;
-                    choseSideButton.Text = @"Blue";
+                    
+                    var clientHello = new ClientHello(nameText.Text, isRed);
+                    var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    socket.Connect(ip, 1997);
+                    socket.Send(clientHello, 0, SocketFlags.None);
+                    socket.Receive()
+                    
+
                 }
                 else
                 {
-                    choseSideButton.BackColor = Color.DarkRed;
-                    choseSideButton.Text = @"Red";
+                    var returnButton = CreateButton(connectingLabel.Bottom, @"Return to menu");
+                    //TODO: возврат в главное меню
+                    //returnButton.Click += (sender, args) => { return; };.
+                    Controls.Add(returnButton);
                 }
-                isRed = !isRed;
+                */
+                nameLabel.Dispose();
+                nameText.Dispose();
+                preferredSideLabel.Dispose();
+                preferredSideButton.Dispose();
+                serverIPLabel.Dispose();
+                serverIPText.Dispose();
+                engageButton.Dispose();
+                Dispose();
             };
-            Controls.Add(choseSideButton);
-            
+
+            SetButtonColor(preferredSideButton, isRed);
+            preferredSideButton.Click += (sender, args) =>
+            {
+                isRed = !isRed;
+                SetButtonColor(preferredSideButton, isRed);
+            };
+
+            Controls.Add(engageButton);
+            Controls.Add(preferredSideLabel);
+            Controls.Add(preferredSideButton);
+            Controls.Add(nameLabel);
+            Controls.Add(nameText);
+            Controls.Add(serverIPLabel);
+            Controls.Add(serverIPText);
+
             //InitializeComponent();
         }
 
@@ -103,6 +101,60 @@ namespace Client.Forms
         {
             base.OnLoad(e);
             Text = @"Space Battle";
+        }
+
+        private Label CreateLabel(int y, string text = null)
+        {
+            var label = new Label();
+            SetControlDefaultOptions(label, y, 64, Color.Transparent, text);
+            label.TextAlign = ContentAlignment.MiddleCenter;
+            if (text != null)
+                label.Text = text;
+            return label;
+        }
+
+        private Button CreateButton(int y, string text = null)
+        {
+            var button = new Button();
+            SetControlDefaultOptions(button, y, 32, Color.Black, text);
+            button.TextAlign = ContentAlignment.MiddleCenter;
+            if (text != null)
+                button.Text = text;
+            return button;
+        }
+
+        private TextBox CreateTextBox(int y, string text = null)
+        {
+            var textBox = new TextBox();
+            SetControlDefaultOptions(textBox, y, 32, Color.Black, text);
+            textBox.TextAlign = HorizontalAlignment.Center;
+            return textBox;
+        }
+
+        private void SetControlDefaultOptions(Control control, int topY, int height, Color backColor, string text)
+        {
+            control.AutoSize = false;
+            control.Location = new Point(0, topY);
+            control.Size = new Size(ClientSize.Width, height);
+            control.BackColor = backColor;
+            control.ForeColor = Color.White;
+            control.Font = new Font(Visual.FontName, 16);
+            if (text != null)
+                control.Text = text;
+        }
+
+        private void SetButtonColor(Button button, bool isRed)
+        {
+            if (isRed)
+            {
+                button.BackColor = Color.DarkRed;
+                button.Text = @"Red";
+            }
+            else
+            {
+                button.BackColor = Color.DarkBlue;
+                button.Text = @"Blue";
+            }
         }
     }
 }
--- Client/Visual.cs
+++ Client/Visual.cs
@@ -12,89 +12,77 @@ namespace Client
         public const int ElementSize = 32;
         public const int IconSize = ElementSize / 2;
         public const string FontName = "Eras Bold ITC";
+        public static readonly Font NumbersFont = new Font(FontName, 10);
 
-        private static readonly Dictionary<Type, Bitmap> RedSprites = new Dictionary<Type, Bitmap>
+        private static readonly Dictionary<Type, int> DrawingPriorities = new Dictionary<Type, int>
         {
-            {typeof(Player), Properties.Resources.PlayerRed},
-            {typeof(FriendlyLaserShot), Properties.Resources.ShotRed},
-            {typeof(EnemyLaserShot), Properties.Resources.ShotBlue}
-        };
-        private static readonly Dictionary<Type, Bitmap> RedSpritesFlipped =
-            RedSprites.ToDictionary(s => s.Key, s => GetFlippedImage(s.Value));
-
-        private static readonly Dictionary<Type, Bitmap> blueSprites = new Dictionary<Type, Bitmap>
-        {
-            {typeof(Player), Properties.Resources.PlayerBlue},
-            {typeof(FriendlyLaserShot), Properties.Resources.ShotBlue},
-            {typeof(EnemyLaserShot), Properties.Resources.ShotRed}
-        };
-        private static readonly Dictionary<Type, Bitmap> BlueSpritesFlipped =
-            blueSprites.ToDictionary(s => s.Key, s => GetFlippedImage(s.Value));
-
-        private static readonly Dictionary<Type, Bitmap> CommonSprites = new Dictionary<Type, Bitmap>
-        {
+            {typeof(Player), 0},
+            {typeof(FriendlyLaserShot), 2},
+            {typeof(EnemyLaserShot), 1}
         };
 
-        public static Bitmap GetFlippedImage(Bitmap sprite)
+        private static Bitmap GetFlippedImage(Bitmap sprite)
         {
             var clone = sprite.Clone() as Bitmap;
             clone.RotateFlip(RotateFlipType.RotateNoneFlipY);
             return clone;
         }
 
-        public static Bitmap GetSpriteForEntity(IEntity entity, bool isBottom, bool isRed)
+        private static IEnumerable<Bitmap> GetSpritesForEntity(IEntity entity, bool isBottom, bool isRed)
         {
-            var entityType = entity.GetType();
-            if (isBottom)
+            var sprites = new List<Bitmap>();
+            switch (entity)
             {
-                if (isRed && RedSprites.TryGetValue(entityType, out var redSprite))
-                    return redSprite;
-                if (blueSprites.TryGetValue(entityType, out var blueSprite))
-                    return blueSprite;
+                case Player p:
+                    sprites.Add(isRed ? Properties.Resources.PlayerRed : Properties.Resources.PlayerBlue);
+                    if (p.ShieldStrength > 0)
+                        sprites.Add(Properties.Resources.Shield);
+                    break;
+                case FriendlyLaserShot s:
+                    sprites.Add(isRed ? Properties.Resources.ShotRed : Properties.Resources.ShotBlue);
+                    break;
+                case EnemyLaserShot s:
+                    sprites.Add(isRed ? Properties.Resources.ShotBlue : Properties.Resources.ShotRed);
+                    break;
+                default:
+                    throw new ArgumentException($"Unknown entity {entity.GetType().Name}");
             }
-            else
-            {
-                if (isRed && RedSpritesFlipped.TryGetValue(entityType, out var redSpriteFlipped))
-                    return redSpriteFlipped; 
-                if (BlueSpritesFlipped.TryGetValue(entityType, out var blueSpriteFlipped))
-                    return blueSpriteFlipped;
-            }
-            if (CommonSprites.TryGetValue(entityType, out var commonSprite))
-                return commonSprite;
 
-            throw new ArgumentException($"Unknown entity {entityType}");
+            if (!isBottom)
+                for (int i = 0; i < sprites.Count; i++)
+                    sprites[i] = GetFlippedImage(sprites[i]);
+            return sprites;
         }
 
-        public static void UpdateDrawingElements(
-            Dictionary<Bitmap, HashSet<Point>> drawingElements,
-            GameState state,
-            bool isBottom,
-            bool isRed,
-            int tick,
-            out Point playerDrawingPosition)
+        public static void UpdateDrawingElements(DrawingElements elements, GameState state, bool isBottom, bool isRed, int tick)
         {
-            drawingElements.Clear();
-            playerDrawingPosition = Point.Empty;
-            foreach (var animation in state.Animations)
+            IOrderedEnumerable<EntityAnimation> sortedAnimations = null;
+            lock (state.Animations)
             {
-                var sprite = GetSpriteForEntity(animation.Entity, isBottom, isRed);
-                var drawingPosition = isBottom ? 
-                    GetShiftedCoordinates(
-                    new Point(animation.BeginActLocation.X, animation.BeginActLocation.Y + state.MapHeight),
-                    animation.Action.DeltaX, animation.Action.DeltaY, tick) :
-                GetShiftedCoordinatesForTopSide(
-                    animation.BeginActLocation,
-                    state.MapWidth,
-                    state.MapHeight,
-                    animation.Action.DeltaX,
-                    animation.Action.DeltaY,
-                    tick);
-                if (animation.Entity is Player)
-                    playerDrawingPosition = drawingPosition;
-                if (drawingElements.TryGetValue(sprite, out var points))
-                    points.Add(drawingPosition);
-                else
-                    drawingElements.Add(sprite, new HashSet<Point> { drawingPosition });
+                sortedAnimations = state.Animations.OrderBy(a => DrawingPriorities[a.Entity.GetType()]);
+            }
+
+            foreach (var animation in sortedAnimations)
+            {
+                var sprites = GetSpritesForEntity(animation.Entity, isBottom, isRed);
+                var drawingLocation = isBottom
+                    ? GetShiftedCoordinates(
+                        new Point(animation.BeginActLocation.X, animation.BeginActLocation.Y + state.MapHeight),
+                        animation.Action.DeltaX, animation.Action.DeltaY, tick)
+                    : GetShiftedCoordinatesForTopSide(
+                        animation.BeginActLocation,
+                        state.MapWidth,
+                        state.MapHeight,
+                        animation.Action.DeltaX,
+                        animation.Action.DeltaY,
+                        tick);
+                foreach (var sprite in sprites)
+                {
+                    if (elements.Images.TryGetValue(sprite, out var points))
+                        points.Add(drawingLocation);
+                    else
+                        elements.Images.Add(sprite, new HashSet<Point> { drawingLocation });
+                }
             }
         }
 
@@ -104,7 +92,7 @@ namespace Client
             int topSideMapHeight,
             int localDeltaX,
             int localDeltaY,
-            int tick) 
+            int tick)
             =>
             GetShiftedCoordinates(
                 ConvertCoordinatesToTopSide(localCoordinates, topSideMapWidth, topSideMapHeight),
@@ -118,11 +106,76 @@ namespace Client
             var yShift = tick * 4 * deltaY;
             return new Point(
                 coordinates.X * ElementSize + xShift,
-                (coordinates.Y  + 1) * ElementSize + yShift);
+                (coordinates.Y + 1) * ElementSize + yShift);
 
         }
 
-        private static Point ConvertCoordinatesToTopSide(Location bottomSideCoordinates, int mapWidth, int mapHeight) => 
+        private static Point ConvertCoordinatesToTopSide(Location bottomSideCoordinates, int mapWidth, int mapHeight) =>
             new Point(mapWidth - bottomSideCoordinates.X - 1, mapHeight - bottomSideCoordinates.Y - 1);
+
+        public static void UpdatePlayerHUD(DrawingElements hud, Player player, Size clientSize, bool isBottom)
+        {
+            var middleX = clientSize.Width / 2;
+            int numericY;
+            int scaledY;
+            if (isBottom)
+            {
+                numericY = clientSize.Height - ElementSize;
+                scaledY = numericY + IconSize;
+            }
+            else
+            {
+                numericY = IconSize;
+                scaledY = 0;
+            }
+
+            UpdateHUDIcons(hud, clientSize, middleX, numericY, scaledY);
+
+            UpdateHUDNumber(hud, player.Armor.ToString(), IconSize, numericY, true);
+            UpdateHUDNumber(hud, $"{(int)(player.ShieldStrength * 100)}%", clientSize.Width - ElementSize, numericY, false);
+
+            var rectangleSize = new Size(middleX / 20 - 1, IconSize);
+
+            var leftX = middleX - IconSize - rectangleSize.Width;
+            UpdateHUDNumber(hud, 10.ToString(), leftX, numericY, false); //TODO: Вынести урон в public поле
+            UpdateHUDBar(hud, player.Health, leftX, scaledY, rectangleSize, Brushes.DarkRed, false);
+            UpdateHUDNumber(hud, player.Health.ToString(), leftX, scaledY, false);
+
+            var rightX = middleX + IconSize;
+            UpdateHUDNumber(hud, 25.ToString(), rightX, numericY, true);
+            UpdateHUDBar(hud, player.Energy, rightX, scaledY, rectangleSize, Brushes.Indigo, true);
+            UpdateHUDNumber(hud, player.Energy.ToString(), rightX, scaledY, true);
+        }
+
+        private static void UpdateHUDIcons(DrawingElements hud, Size clientSize, int middleX, int numericY, int scaledY)
+        {
+            hud.Images.Add(Properties.Resources.HUDArmor, new HashSet<Point> { new Point(0, numericY) });
+            hud.Images.Add(Properties.Resources.HUDShield, new HashSet<Point> { new Point(clientSize.Width - IconSize, numericY) });
+            hud.Images.Add(Properties.Resources.HUDHealthDamage, new HashSet<Point> { new Point(middleX - IconSize, numericY) });
+            hud.Images.Add(Properties.Resources.HUDEnergyDamage, new HashSet<Point> { new Point(middleX, numericY) });
+            hud.Images.Add(Properties.Resources.HUDHealth, new HashSet<Point> { new Point(middleX - IconSize, scaledY) });
+            hud.Images.Add(Properties.Resources.HUDEnergy, new HashSet<Point> { new Point(middleX, scaledY) });
+        }
+
+        private static void UpdateHUDBar(DrawingElements hud, int value, int beginX, int beginY, Size rectangleSize, Brush brush, bool toRight)
+        {
+            var deltaX = rectangleSize.Width + 1;
+            if (!toRight)
+                deltaX = -deltaX;
+            for (int i = 0; i < value / 5; i++)
+                hud.Rectangles.Add(new Rectangle(beginX + i * deltaX, beginY, rectangleSize.Width, rectangleSize.Height), brush);
+        }
+
+        private static void UpdateHUDNumber(DrawingElements hud, string stringValue, int beginX, int beginY, bool toRight)
+        {
+            var numberX = beginX;
+            if (!toRight)
+                numberX -= stringValue.Length * 9 - 2;
+            var location = new Point(numberX, beginY);
+            if (hud.Strings.TryGetValue(stringValue, out var value))
+                value.Add(location);
+            else
+                hud.Strings.Add(stringValue, new HashSet<Point> { location });
+        }
     }
 }
